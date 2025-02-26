4fff79f6d512c5f8eda35cc938ec808886dec931
Sean Murphy <sean@iamseanmurphy.com>
2025-02-26 16:17:59 +0000

    2.4.5

--- poll-wp/includes/class-ts_poll.php
+++ poll-wp/includes/class-ts_poll.php
@@ -73,7 +73,7 @@ class TS_Poll {
 		if ( defined( 'TS_POLL_VERSION' ) ) {
 			$this->version = TS_POLL_VERSION;
 		} else {
-			$this->version = '2.4.4';
+			$this->version = '2.4.5';
 		}
 		$this->plugin_name = 'TS Poll';
 		$this->load_dependencies();
